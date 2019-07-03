5c8e43a981851ab738f57adb934d5f181d440083
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-07-03 17:52:11 +0200

    tensorflow update

--- GIRAFFE/code/neural_net.py
+++ GIRAFFE/code/neural_net.py
@@ -1,8 +1,9 @@
 '''
-Created by the GiraffeTools Keras generator.
+Created by the GiraffeTools Tensorflow generator.
 Warning, here be dragons.
 
 '''
+
 
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D
@@ -18,72 +19,72 @@ def NeuralNet(
     metrics=['accuracy']
   ):
 
-    dense = Dense(
+    conv2d = Conv2D(
+      32,
+      (3,3),
+      strides=(1, 1),
+      padding='valid',
+      dilation_rate=(1, 1),
+      use_bias=True,
+      kernel_initializer='glorot_uniform',
+      bias_initializer='zeros',
+      name='conv2d'
+    )
+
+    conv2d_1 = Conv2D(
+      32,
+      (3,3),
+      strides=(1, 1),
+      padding='valid',
+      dilation_rate=(1, 1),
+      use_bias=True,
+      kernel_initializer='glorot_uniform',
+      bias_initializer='zeros',
+      name='conv2d_1'
+    )(conv2d)
+
+    max_pooling2d = MaxPooling2D(
+      pool_size=(2, 2),
+      padding='valid',
+      name='max_pooling2d'
+    )(conv2d_1)
+
+    dropout_1 = Dropout(
+      0.25,
+      name='dropout_1'
+    )(max_pooling2d)
+
+    flatten = Flatten(
+      name='flatten'
+    )(dropout_1)
+
+    dense_1 = Dense(
+      128,
+      use_bias=True,
+      kernel_initializer='glorot_uniform',
+      bias_initializer='zeros',
+      name='dense_1'
+    )(flatten)
+
+    dropout_3 = Dropout(
+      0.5,
+      name='dropout_3'
+    )(dense_1)
+
+    dense_2 = Dense(
       10,
       activation='softmax',
       use_bias=True,
       kernel_initializer='glorot_uniform',
       bias_initializer='zeros',
-      name='dense'
-    )
-
-    dropout = Dropout(
-      0.5,
-      name='dropout'
-    )(dense)
-
-    dense_2 = Dense(
-      128,
-      use_bias=True,
-      kernel_initializer='glorot_uniform',
-      bias_initializer='zeros',
       name='dense_2'
-    )(dropout)
-
-    flatten_1 = Flatten(
-      name='flatten_1'
-    )(dense_2)
-
-    dropout_2 = Dropout(
-      0.25,
-      name='dropout_2'
-    )(flatten_1)
-
-    max_pooling2_d_1 = MaxPooling2D(
-      pool_size=(2, 2),
-      padding='valid',
-      name='max_pooling2_d_1'
-    )(dropout_2)
-
-    conv2_d = Conv2D(
-      32,
-      (3,3),
-      strides=(1, 1),
-      padding='valid',
-      dilation_rate=(1, 1),
-      use_bias=True,
-      kernel_initializer='glorot_uniform',
-      bias_initializer='zeros',
-      name='conv2_d'
-    )(max_pooling2_d_1)
-
-    conv2_d_2 = Conv2D(
-      32,
-      (3,3),
-      strides=(1, 1),
-      padding='valid',
-      dilation_rate=(1, 1),
-      use_bias=True,
-      kernel_initializer='glorot_uniform',
-      bias_initializer='zeros',
-      name='conv2_d_2'
-    )(conv2_d)
+    )(dropout_3)
 
 
     # Creating model
     _model = tf.keras.models.Model(
-      inputs  = [conv2_d_2],
-      outputs = [dense]
+      inputs  = [conv2d],
+      outputs = [dense_2]
     )
 
     _model.compile(
